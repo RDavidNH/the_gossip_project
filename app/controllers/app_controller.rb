3a2e56674d804d1219e68a8f548555cdd4306abe
Rabedaoro David N H <rdavidnh@gmail.com>
2019-10-29 00:48:35 +0300

    update

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,6 +1,26 @@
 class AppController < ApplicationController
 
-  def team
-    
+  def index
+    @potins = Gossip.all
   end
+  
+  def show
+    @potin = Gossip.find(params[:id])
+  end
+  
+  def show_user
+    @user = User.find params[:id]
+    p @user
+  end
+
+  def welcome
+    @user = params[:name]
+  end
+
+  def team
+  end
+  
+  def contact
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   
+  root 'app#index'
+  get '/gossip/:id', to: 'app#show', as: 'show'
+  get '/user/:id', to: 'app#show_user', as: 'show_user'
+  get '/welcome/:name', to: 'app#welcome'
   get '/team', to: 'app#team'
+  get '/contact', to: 'app#contact'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
