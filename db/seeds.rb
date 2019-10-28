46eca1881953ad257717b7d032a84a7a68f01167
R David N H <rdavidnh@gmail.com>
2019-10-28 15:03:45 +0300

    added models, migrations, seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,73 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#require 'faker'
+
+cities = []
+users = []
+tags = []
+
+# create 10 users with 10 cities
+10.times do |i|
+
+  city = City.create(
+    name: Faker::Address.city,
+    zip_code: Faker::Address.zip_code
+  )
+
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    description: Faker::Lorem.sentence(word_count: 12),
+    email: Faker::Internet.email,
+    age: Faker::Number.within(range: 15..70),
+    city: city
+  )
+  
+  users << user
+  
+end
+
+# create 10 tags
+10.times do |i|
+  tag = Tag.create(
+    title: Faker::Lorem.word
+  )
+  
+  tags << tag
+end
+
+# create 20 gossips with 1 tag and 1 comment each
+20.times do |i|
+  gossip = Gossip.create(
+    title: Faker::Lorem.word,
+    content: Faker::Lorem.sentence(word_count: 15),
+    user: users[rand(0..9)]
+  )
+  
+  
+  gossip_tag = GossipTag.create(
+    gossip: gossip,
+    tag: tags[rand(0..9)]
+  )
+  
+  comment = Comment.create(
+    content: Faker::Lorem.sentence(word_count: 10),
+    user: users[rand(0..9)],
+    gossip: gossip
+  )
+  
+end
+
+
+
+
+
+
+
+
+
+
+
+
