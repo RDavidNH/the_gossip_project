6ec857889ec1cfc0f96ce3db4b6b9ec647bb2ff8
R David N H <rdavidnh@gmail.com>
2019-10-30 11:42:07 +0300

    added create/read actions + validations to User model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   root 'app#index'
   resources :gossips
+  resources :users
   
   #get '/gossip/:id', to: 'app#show', as: 'show'
   #get '/user/:id', to: 'app#show_user', as: 'show_user'
