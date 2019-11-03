b4a1651274182ea91bc2dcfe0ef3a34d3f156a2a
Rabedaoro David N H <rdavidnh@gmail.com>
2019-11-03 20:33:53 +0300

    added authentication + update

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,7 +1,17 @@
 class AppController < ApplicationController
 
+  before_action :authenticate_user, only: [:index]
+  
+  def authenticate_user
+    unless current_user
+      flash[:danger] = "Please log in."
+      redirect_to new_session_path
+    end
+  end
+  
   def index
     @potins = Gossip.all
+    @cu = User.find(session[:user_id])
   end
   
   def show
